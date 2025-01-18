d4c3851d4a5ee65e9cd852ade60907eb020b0c7e
2shoneycom <2shoneycom@gmail.com>
2025-01-18 20:05:39 +0900

    (feature/GL)[이승헌]족보 판단 기능 수정
    PokerHandEvaluator.cs 파일 내에서만 수정
    Ace 카드가 족보 판단에서 변수가 될 수 있던 점을 고려하여
    Ace 카드가 1로써 한번, 14로써 한번, 총 두번 족보 판단을 하게끔 수정

--- [Unity]PokerGame/Assets/Scripts/PokerHandEvaluator.cs
+++ [Unity]PokerGame/Assets/Scripts/PokerHandEvaluator.cs
@@ -5,80 +5,61 @@ using UnityEngine;
 
 public class PokerHandEvaluator : MonoBehaviour
 {
-    public int[] idxs = new int[5];    // 이 코드를 호출 할 때, 여기에 카드 5장의 인덱스를 줄거임. 그래야함.
+    public int[] idxs = new int[5];         // 이 코드를 호출 할 때, 여기에 카드 5장의 인덱스를 줄거임. 그래야함.
     [SerializeField]
-    private char[] shapes = new char[5]; // idxs 배열을 Card System 쪽에 있는 무늬 판별기에 넣고 무늬를 얻어옴.
+    private char[] shapes = new char[5];    // idxs 배열을 Card System 쪽에 있는 무늬 판별기에 넣고 무늬를 얻어옴.
     [SerializeField]
-    private int[] nums = new int[5]; // idx 배열을 Card System 쪽에 있는 숫자 판별기에 넣고 숫자를 얻어옴.
-    private int handRank = -1;   // 이 5장의 족보 순위 ex) 0: highcard, 1: onepair ... 8: straight flush
-    private int handScore = 0;  // 해당 족보안에서의 카드 점수 (동점 판별용)
-    private bool is_flush = true;
-    private bool is_straight = false;
+    private int[] nums = new int[5];        // idx 배열을 Card System 쪽에 있는 숫자 판별기에 넣고 숫자를 얻어옴.
+    private int handRank;   // 이 5장의 족보 순위 ex) 0: highcard, 1: onepair ... 8: straight flush
+    private int handScore; // 해당 족보안에서의 카드 점수 (동점 판별용)
+    private bool is_flush;
+    private bool is_straight;
     
     void Start()
     {
-        // Start에서 Card System 쪽에 있는 숫자를 보고 무늬, 숫자 판단하는 거 가져오기
-        
-        // nums 내림차순 정렬
-        Array.Sort(nums);
-        Array.Reverse(nums);
+        /*
+        Start에서 Card System 쪽에 있는 숫자를 보고 무늬, 숫자 판단하는 거 가져오는 코드
+        */
 
-        // 플러쉬, 스트레이트 여부는 미리 구해놓기
-        CheckFlush();
-        CheckStraight();
+        handRank = -1;
+        handScore = 0;
 
-        // 본격 족보 판단
-        EvaluteHand();
-
-        Debug.Log("handRank: " + handRank + ", handScore: " + handScore);
-    }
-
-    private void EvaluteHand ()
-    {
-        if (IsStraightFlush()) {
-            Debug.Log("스트레이트 플러쉬입니다.");
-            return;
-        }
-        else if (IsFourCard()) {
-            Debug.Log("포카드입니다.");
-            return;
-        }
-        else if (IsFullHouse()) {
-            Debug.Log("풀하우스입니다.");
-            return;
-        }
-        else if (IsFlush()) {
-            Debug.Log("플러쉬입니다.");
-            return;
-        }
-        else if (IsStraight()) {
-            Debug.Log("스트레이트입니다.");
-            return;
-        }
-        else if (IsTriple()) {
-            Debug.Log("트리플입니다.");
-            return;
-        }
-        else if (IsTwoPair()) {
-            Debug.Log("투페어입니다.");
-            return;
-        }
-        else if (IsOnePair()) {
-            Debug.Log("원페어입니다.");
-            return;
-        }
-        else {
-            // 하이 카드
-            Debug.Log("하이카드입니다.");
-            handRank = 0;
-            for (int i = 0; i < 5; i++) {
-                handScore *= 10;
-                handScore += nums[i];
+        // 족보 판단을 두 번 행할꺼임 (A 때문에..)
+        for (int i = 0; i < 2; i++) {
+            if (i == 1) {
+                for (int j = 0; j < 5; j++) {
+                    // 두 번째 족보 체크에서는 A 카드를 14로 여김
+                    if (nums[j]==1) {
+                        nums[j]=14;
+                    }
+                }
             }
+
+            // nums 내림차순 정렬
+            Array.Sort(nums);
+            Array.Reverse(nums);
+
+            // 플러쉬, 스트레이트 여부는 미리 구해놓기
+            SetFlush();
+            SetStraight();
+
+            // 본격 족보 판단
+            var (curRank, curScore) = GetRank();
+
+            if (curRank > handRank) {
+                handScore = curScore;
+            }
+            else if (curRank == handRank) {
+                handScore = Math.Max(handScore, curScore);
+            }
+            handRank = Math.Max(handRank, curRank);
+
+            Debug.Log("handRank: " + handRank + ", handScore: " + handScore);
         }
+
     }
 
-    private void CheckFlush ()
+    private void SetFlush ()
     {
         is_flush = true;
         // 5 장을 순회하는 동안 한 번이라도 서로 다르면 false
@@ -89,178 +70,204 @@ public class PokerHandEvaluator : MonoBehaviour
         }
     }
 
-    private void CheckStraight ()
+    private void SetStraight ()
     {
+        is_straight = false;
+        // ex) 5,4,3,2,1
         if (nums[0]==nums[1]+1 && nums[1]==nums[2]+1
         && nums[2]==nums[3]+1 && nums[3]==nums[4]+1) {
             is_straight = true;
         }
     }
 
-    private bool IsStraightFlush ()
+    private Tuple<int,int> GetRank ()
     {
-        bool tf = false;
+        int scr = -1;
+
+        scr = IsStraightFlush();
+        if (scr > 0) {
+            Debug.Log("스트레이트 플러쉬입니다.");
+            return Tuple.Create(8,scr);
+        }
         
-        if (is_flush && is_straight) {
-            tf = true;
-            handRank = 8;
-            handScore = nums[0];    // 스트레이트 계열은 제일 높은 한 장만 보면 됨
+        scr = IsFourCard();
+        if (scr > 0) {
+            Debug.Log("포카드입니다.");
+            return Tuple.Create(7,scr);
         }
 
-        return tf;
+        scr = IsFullHouse();
+        if (scr > 0) {
+            Debug.Log("풀하우스입니다.");
+            return Tuple.Create(6,scr);
+        }
+
+        scr = IsFlush();
+        if (scr > 0) {
+            Debug.Log("플러쉬입니다.");
+            return Tuple.Create(5,scr);
+        }
+
+        scr = IsStraight();
+        if (scr > 0) {
+            Debug.Log("스트레이트입니다.");
+            return Tuple.Create(4,scr);
+        }
+
+        scr = IsTriple();
+        if (scr > 0) {
+            Debug.Log("트리플입니다.");
+            return Tuple.Create(3,scr);
+        }
+
+        scr = IsTwoPair();
+        if (scr > 0) {
+            Debug.Log("투페어입니다.");
+            return Tuple.Create(2,scr);
+        }
+
+        scr = IsOnePair();
+        if (scr > 0) {
+            Debug.Log("원페어입니다.");
+            return Tuple.Create(1,scr);
+        }
+
+        // 하이 카드
+        Debug.Log("하이카드입니다.");
+        scr = 0;
+        for (int i = 0; i < 5; i++) {
+            scr *= 100;
+            scr += nums[i];
+        }
+        return Tuple.Create(0,scr);
     }
 
-    private bool IsFourCard ()
+    private int IsStraightFlush ()
     {
-        bool tf = false;
+        int scr = -1;
+        
+        if (is_flush && is_straight) {
+            scr = nums[0];    // 스트레이트 계열은 제일 높은 한 장만 보면 됨
+        }
+
+        return scr;
+    }
+
+    private int IsFourCard ()
+    {
+        int scr = -1;
 
         if (nums[0]==nums[1] && nums[1]==nums[2] && nums[2]==nums[3]) {
             // 앞의 4장이 같은 경우
-            tf = true;
-            handRank = 7;
-            handScore = nums[0];    // 포카드는 포카드를 이루는 한 장을 비교하면 됨
+            scr = nums[0];    // 포카드는 포카드를 이루는 한 장을 비교하면 됨
         }
         else if (nums[1]==nums[2] && nums[2]==nums[3] && nums[3]==nums[4]) {
             // 뒤의 4장이 같은 경우
-            tf = true;
-            handRank = 7;
-            handScore = nums[4];    // 포카드는 포카드를 이루는 한 장을 비교하면 됨
+            scr = nums[4];    // 포카드는 포카드를 이루는 한 장을 비교하면 됨
         }
 
-        return tf;
+        return scr;
     }
 
-    private bool IsFullHouse ()
+    private int IsFullHouse ()
     {
-        bool tf = false;
+        int scr = -1;
 
         if (nums[0]==nums[1] && nums[1]==nums[2] && nums[3]==nums[4]) {
             // 앞의 3장이 같고 뒤의 2장이 같은 경우
-            tf = true;
-            handRank = 6;
-            handScore = nums[0];    // 풀하우스는 트리플을 이루는 한 장을 비교하면 됨
+            scr = nums[0];    // 풀하우스는 트리플을 이루는 한 장을 비교하면 됨
         }
         else if (nums[0]==nums[1] && nums[2]==nums[3] && nums[3]==nums[4]) {
             // 앞의 2장이 같고 뒤의 3장이 같은 경우
-            tf = true;
-            handRank = 6;
-            handScore = nums[4];    // 풀하우스는 트리플을 이루는 한 장을 비교하면 됨
+            scr = nums[4];    // 풀하우스는 트리플을 이루는 한 장을 비교하면 됨
         }
 
-        return tf;
+        return scr;
     }
 
-    private bool IsFlush ()
+    private int IsFlush ()
     {
-        bool tf = false;
-
+        int scr = -1;
         if (is_flush) {
-            tf = true;
-            handRank = 5;
             for (int i = 0; i < 5; i++) {
                 // 플러쉬끼리는 하이카드 방식으로 비교해야함
-                handScore *= 10;
-                handScore += nums[i];
+                scr *= 100;
+                scr += nums[i];
             }
         }
-
-        return tf;
+        return scr;
     }
 
-    private bool IsStraight ()
+    private int IsStraight ()
     {
-        bool tf = false;
-
+        int scr = -1;
         if (is_straight) {
-            tf = true;
-            handRank = 4;
-            handScore = nums[0];    // 스트레이트는 제일 높은 한 장만 비교하면 됨
+            scr = nums[0];    // 스트레이트는 제일 높은 한 장만 비교하면 됨
         }
-
-        return tf;
+        return scr;
     }
 
-    private bool IsTriple ()
+    private int IsTriple ()
     {
-        bool tf = false;
+        int scr = -1;
 
         if (nums[0]==nums[1] && nums[1]==nums[2]) {
             // 앞의 3장이 같은 경우
-            tf = true;
-            handRank = 3;
-            handScore = nums[0];    // 트리플은 트리플을 이루는 카드 한 장만 비교하면 됨
+            scr = nums[0];    // 트리플은 트리플을 이루는 카드 한 장만 비교하면 됨
         }
         else if (nums[1]==nums[2] && nums[2]==nums[3]) {
             // 중간 3장이 같은 경우
-            tf = true;
-            handRank = 3;
-            handScore = nums[1];    // 트리플은 트리플을 이루는 카드 한 장만 비교하면 됨
+            scr = nums[1];    // 트리플은 트리플을 이루는 카드 한 장만 비교하면 됨
         }
         else if (nums[2]==nums[3] && nums[3]==nums[4]) {
             // 뒤의 3장이 같은 경우
-            tf = true;
-            handRank = 3;
-            handScore = nums[2];    // 트리플은 트리플을 이루는 카드 한 장만 비교하면 됨
+            scr = nums[2];    // 트리플은 트리플을 이루는 카드 한 장만 비교하면 됨
         }
 
-        return tf;
+        return scr;
     }
 
-    private bool IsTwoPair ()
+    private int IsTwoPair ()
     {
-        bool tf = false;
+        int scr = -1;
 
         if (nums[0]==nums[1] && nums[2]==nums[3]) {
             // 앞의 두 쌍이 같은 경우 ex) 7,7,4,4,1
-            tf = true;
-            handRank = 2;
-            handScore = 100*nums[0]+10*nums[2]+nums[4];
+            scr = 10000*nums[0]+100*nums[2]+nums[4];
         }
         else if (nums[0]==nums[1] && nums[3]==nums[4]) {
             // 앞의 한 쌍, 뒤의 한 쌍이 같은 경우 ex) 7,7,4,1,1
-            tf = true;
-            handRank = 2;
-            handScore = 100*nums[0]+10*nums[3]+nums[2];
+            scr = 10000*nums[0]+100*nums[3]+nums[2];
         }
         else if (nums[1]==nums[2] && nums[3]==nums[4]) {
             // 뒤의 두 쌍이 같은 경우 ex) 7,4,4,1,1
-            tf = true;
-            handRank = 2;
-            handScore = 100*nums[1]+10*nums[3]+nums[0];
+            scr = 10000*nums[1]+100*nums[3]+nums[0];
         }
 
-        return tf;
+        return scr;
     }
 
-    private bool IsOnePair ()
+    private int IsOnePair ()
     {
-        bool tf = false;
+        int scr = -1;
 
         if (nums[0]==nums[1]) {
             // ex) 7,7,5,3,1
-            tf = true;
-            handRank = 1;
-            handScore = 1000*nums[0]+100*nums[2]+10*nums[3]+nums[4];
+            scr = 1000000*nums[0]+10000*nums[2]+100*nums[3]+nums[4];
         }
         else if (nums[1]==nums[2]) {
             // ex) 7,5,5,3,1
-            tf = true;
-            handRank = 1;
-            handScore = 1000*nums[1]+100*nums[0]+10*nums[3]+nums[4];
+            scr = 1000000*nums[1]+10000*nums[0]+100*nums[3]+nums[4];
         }
         else if (nums[2]==nums[3]) {
             // ex) 7,5,3,3,1
-            tf = true;
-            handRank = 1;
-            handScore = 1000*nums[2]+100*nums[0]+10*nums[1]+nums[4];
+            scr = 1000000*nums[2]+10000*nums[0]+100*nums[1]+nums[4];
         }
         else if (nums[3]==nums[4]) {
             // ex) 7,5,3,1,1
-            tf = true;
-            handRank = 1;
-            handScore = 1000*nums[3]+100*nums[0]+10*nums[1]+nums[2];
+            scr = 100000000*nums[3]+10000*nums[0]+100*nums[1]+nums[2];
         }
 
-        return tf;
+        return scr;
     }
 }
